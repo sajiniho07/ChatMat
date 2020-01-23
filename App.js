adeb92c793940e7e2ebe14f04b6f1923763c6df5
sajiniho <sajadkamali017@gmail.com>
2020-01-24 01:03:36 +0330

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, {Component, Fragment} from 'react';
+import socketIOClient from 'socket.io-client';
+import {Text, View, StyleSheet } from 'react-native'
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            response: false,
+            endpoint: 'http://localhost:8080',
+        };
+    }
+    componentDidMount() {
+        const {endpoint} = this.state;
+        const socket = socketIOClient(endpoint);
+        socket.on('FromAPI', data => this.setState({response: data}));
+    }
+
+    render() {
+        const {response} = this.state;
+        return (
+            <Fragment>
+                <View style={styles.container}>
+                    {
+                        response ? <Text>The temperature in Shiraz is: {response} °C</Text>
+                        : <Text>Loading...</Text>
+                    }
+                </View>
+            </Fragment>
+        );
+    }
+}
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 8080;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server); // < Interesting!
+
+io.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+        () => getApiAndEmit(socket),
+        10000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get(
+            "https://api.darksky.net/forecast/1ed1ddbb4a7ab6edb9159c0a24db77ff/42.3601,-71.0589"
+        ); // Getting the data from DarkSky
+        socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
